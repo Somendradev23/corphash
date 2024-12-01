f59eb8de2c1196aa21580fb20c081c98264637a1
Somendradev23 <somendradev23@gmail.com>
2024-12-01 12:32:50 +0530

    code updated

--- contactus.php
+++ contactus.php
@@ -2,9 +2,9 @@
 
 //  PDO Connection
 $servername = "localhost";
-$username = "u511675806_corphash";
+$username = "u616153323_corphash";
 $password = "qVL8~q7~";
-$dbname = "u511675806_corphash";
+$dbname = "u616153323_corphash";
 
 // Create connection
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -22,12 +22,28 @@ if ($conn->connect_error) {
 
 // save data
 
-try{
+try {
     if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
         $name = $_POST['name'];
         $email = $_POST['email'];
-        $message = $_POST['message'];
-        
+        $message = $email . " says:" . $_POST['message'];
+
+
+        // Email configuration
+        $to = "somendradev23@gmail.com"; // Recipient's email address
+        $subject = "Contact Form"; // Email subject
+        $headers = "From: contact@corphash.net\r\n"; // Sender's email address
+        $headers .= "Reply-To: contact@corphash.net\r\n"; // Reply-to address
+        $headers .= "X-Mailer: PHP/" . phpversion(); // Information about the email source
+
+        // Send email
+        if (mail($to, $subject, $message, $headers)) {
+            //echo "Email sent successfully.";
+        } else {
+            //echo "Failed to send email.";
+        }
+
+
         $stmt = $conn->prepare("INSERT INTO contactus(name, email, message) VALUES (:name, :email, :message)");
         $stmt->execute(array(
             ':name' => $name,
@@ -40,16 +56,13 @@ try{
             "message" => "Message sent successfully",
 
         ));
-
-    }else{
+    } else {
         echo json_encode(array(
             "success" => false,
             "message" => "Please fill all the fields",
         ));
-
     }
-
-}catch(Exception $e){
+} catch (Exception $e) {
     echo $e->getMessage();
 }
 
