05ad1f80778adf80285ccc66d1af952b063bd908
Somendradev23 <somendradev23@gmail.com>
2024-12-01 18:36:10 +0530

    googla analysis added

--- comp/header.php
+++ comp/header.php
@@ -1,75 +1,88 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" href="/assets/images/www.svg" type="image/x-icon" />
-    <title>CorpHash | <?= $page_title ?></title>
 
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-    <link rel="stylesheet" href="assets/css/materialize.css" />
+<head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-W3RRTWCKNN"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
 
-    <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
-    <link rel="stylesheet" href="assets/css/owl.theme.green.min.css" />
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
 
-    <link rel="stylesheet" href="assets/css/animate.css" />
+    gtag('config', 'G-W3RRTWCKNN');
+  </script>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="shortcut icon" href="/assets/images/www.svg" type="image/x-icon" />
+  <title>CorpHash | <?= $page_title ?></title>
 
-    <link rel="stylesheet" type="text/css" href="assets/css/tox-progress.css" />
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
-  </head>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+  <link rel="stylesheet" href="assets/css/materialize.css" />
 
-  <body>
-    <div class="navbar-fixed">
-      <nav style="background: transparent">
-        <div class="nav-wrapper">
-          <div class="container">
-            <a href="/" class="brand-logo">CorpHash</a>
-            <a href="#" data-target="mobile-demo" class="sidenav-trigger white-text"><i class="material-icons">menu</i></a>
-            <ul class="right hide-on-med-and-down">
-              <li><a href="/about">About Us</a></li>
-              <li><a href="/index#service">Services</a></li>
-              <li>
-                <a href="#" class="dropdown-trigger" data-target="Experties1">
-                  Experties
-                  <i class="material-icons right">arrow_drop_down</i>
-                </a>
-              </li>
-              <li><a href="/contact">Contact Us</a></li>
-              <li><a href="tel:+918953173300" class="btn orange">Get Quote</a></li>
-            </ul>
-          </div>
+  <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
+  <link rel="stylesheet" href="assets/css/owl.theme.green.min.css" />
+
+  <link rel="stylesheet" href="assets/css/animate.css" />
+
+  <link rel="stylesheet" type="text/css" href="assets/css/tox-progress.css" />
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+</head>
+
+<body>
+  <div class="navbar-fixed">
+    <nav style="background: transparent">
+      <div class="nav-wrapper">
+        <div class="container">
+          <a href="/" class="brand-logo">CorpHash</a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger white-text"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="/about">About Us</a></li>
+            <li><a href="/index#service">Services</a></li>
+            <li>
+              <a href="#" class="dropdown-trigger" data-target="Experties1">
+                Experties
+                <i class="material-icons right">arrow_drop_down</i>
+              </a>
+            </li>
+            <li><a href="/contact">Contact Us</a></li>
+            <li><a href="tel:+918953173300" class="btn orange">Get Quote</a></li>
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
+  </div>
 
-    <ul class="sidenav" id="mobile-demo">
-      <li><a href="/about">About Us</a></li>
-      <li><a href="/index#service">Services</a></li>
-      <li>
-        <a href="#" class="dropdown-trigger" data-target="Experties2">
-          Experties
-          <i class="material-icons right">arrow_drop_down</i>
-        </a>
-      </li>
-      <li><a href="/contact">Contact Us</a></li>
-      <li><a href="tel:+9125615672" class="btn orange">Get Quote</a></li>
-    </ul>
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="/about">About Us</a></li>
+    <li><a href="/index#service">Services</a></li>
+    <li>
+      <a href="#" class="dropdown-trigger" data-target="Experties2">
+        Experties
+        <i class="material-icons right">arrow_drop_down</i>
+      </a>
+    </li>
+    <li><a href="/contact">Contact Us</a></li>
+    <li><a href="tel:+9125615672" class="btn orange">Get Quote</a></li>
+  </ul>
 
-    <!-- Dropdown Experties -->
-    <ul id="Experties1" class="dropdown-content">
-      <li><a class="blue-text accent-3" href="#!">Web App Development</a></li>
-      <li><a class="blue-text accent-3" href="#!">Android App Development</a></li>
-      <li><a class="blue-text accent-3" href="#!">Social Media Marketing</a></li>
-      <li><a class="blue-text accent-3" href="#!">SEO</a></li>
-      <li><a class="blue-text accent-3" href="#!">E-Commerce Web Development</a></li>
-      <li><a class="blue-text accent-3" href="#!">Enter Application (ERP)</a></li>
-    </ul>
+  <!-- Dropdown Experties -->
+  <ul id="Experties1" class="dropdown-content">
+    <li><a class="blue-text accent-3" href="#!">Web App Development</a></li>
+    <li><a class="blue-text accent-3" href="#!">Android App Development</a></li>
+    <li><a class="blue-text accent-3" href="#!">Social Media Marketing</a></li>
+    <li><a class="blue-text accent-3" href="#!">SEO</a></li>
+    <li><a class="blue-text accent-3" href="#!">E-Commerce Web Development</a></li>
+    <li><a class="blue-text accent-3" href="#!">Enter Application (ERP)</a></li>
+  </ul>
 
-    <ul id="Experties2" class="dropdown-content">
-      <li><a class="blue-text accent-3" href="#!">Web App Development</a></li>
-      <li><a class="blue-text accent-3" href="#!">Android App Development</a></li>
-      <li><a class="blue-text accent-3" href="#!">Social Media Marketing</a></li>
-      <li><a class="blue-text accent-3" href="#!">SEO</a></li>
-      <li><a class="blue-text accent-3" href="#!">E-Commerce Web Development</a></li>
-      <li><a class="blue-text accent-3" href="#!">Enter Application (ERP)</a></li>
-    </ul>
+  <ul id="Experties2" class="dropdown-content">
+    <li><a class="blue-text accent-3" href="#!">Web App Development</a></li>
+    <li><a class="blue-text accent-3" href="#!">Android App Development</a></li>
+    <li><a class="blue-text accent-3" href="#!">Social Media Marketing</a></li>
+    <li><a class="blue-text accent-3" href="#!">SEO</a></li>
+    <li><a class="blue-text accent-3" href="#!">E-Commerce Web Development</a></li>
+    <li><a class="blue-text accent-3" href="#!">Enter Application (ERP)</a></li>
+  </ul>
